ef5132fdba1b69946f01ab9d3b20b6892f426ec2
freeland120 <freeland120@naver.com>
2020-06-11 17:48:51 +0900

    2020.6.11 Todolist

---
+++ 99_js_data.js
@@ -0,0 +1,9 @@
+/* 
+
+-primitive : ★Number, Boolean, String★
+
+- reference : 여러개가 묶이는 걸 생각하면 됨
+    ★array, Object★
+
+
+*/
---
+++ cli-test/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to BlockChain World App✅" />
+
+    <!-- 3.컴포넌트를 템플릿에 추가한다. -->
+    <TodoList :todos="todos" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+//1. TodoList.vue를 import한다.
+import TodoList from "./components/TodoList";
+
+export default {
+  name: "App",
+  components: {
+    //2.컴포넌트를 등록한다.
+    TodoList,
+    HelloWorld
+  },
+  data() {
+    return {
+      todos: [
+        { id: 1, content: "Vue.js 복습", isCompleted: false },
+        { id: 2, content: "IBK기업은행", isCompleted: false },
+        { id: 3, content: "NH농협은행", isCompleted: false },
+        { id: 4, content: "KB국민은행", isCompleted: false },
+        { id: 5, content: "우리은행", isCompleted: false }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
